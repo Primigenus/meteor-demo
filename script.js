ba23f0c3e98a3637392de99c9ed7c58df2180857
Rahul Choudhury <rchoudhury@gmail.com>
2015-01-21 16:51:17 -0800

    Handle large numbers of meetup attendees :)

--- script.js
+++ script.js
@@ -24,7 +24,16 @@ if (Meteor.isClient) {
   Meteor.startup(function() {
     Session.setDefault("sortby", "name");
     Session.setDefault("sortdir", 1);
+    Session.setDefault("numResults", 20);
   });
+
+  // Load more results when we scroll to the bottom of the page
+  var throttled = _.throttle(function() {
+    var atBottom = $(document).height() - ($(window).height() + $(document).scrollTop()) === 0;
+    if (atBottom)
+      Session.set("numResults", 5 + Session.get("numResults"))
+  }, 300);
+  $(window).scroll(throttled);
 
   Template.people.helpers({
 
@@ -35,7 +44,7 @@ if (Meteor.isClient) {
       var sortdir = Session.get("sortdir");
       var sort = {};
       sort[sortby] = sortdir;
-      return People.find({}, {sort: sort});
+      return People.find({}, {sort: sort, limit: Session.get("numResults")});
     },
 
     // How many people there are
@@ -86,10 +95,10 @@ if (Meteor.isClient) {
     // Draw an arrow up or down depending on whether and how
     // we're sorting this column
     sortby: function(col) {
-      if (Session.equals("sortby", col) && 
+      if (Session.equals("sortby", col) &&
         Session.equals("sortdir", 1))
         return "&uarr;"
-      else if (Session.equals("sortby", col) && 
+      else if (Session.equals("sortby", col) &&
         Session.equals("sortdir", -1))
         return "&darr;";
       else return "";
@@ -129,7 +138,7 @@ if (Meteor.isClient) {
       Session.set("sortdir", Session.get("sortdir") * -1);
     }
   });
-  
+
 }
 
 
@@ -148,7 +157,7 @@ if (Meteor.isServer) {
     // only allow updates that increment plusones by 1 and
     // disallow upvoting yourself
     update: function(userId, docs, fields, modifier) {
-      var isMe = docs[0].name.toLowerCase() === 
+      var isMe = docs[0].name.toLowerCase() ===
         Meteor.user().profile.name.toLowerCase();
       var isPlusOne = _.keys(modifier).length == 1
         && modifier["$inc"] && modifier["$inc"].plusones == 1;
@@ -213,36 +222,9 @@ if (Meteor.isServer) {
       People.remove({});
 
       // Prefill the database with people who signed up for this talk :)
-      var people = [
-        "Albert Netymk",
-        "Albin Larsson",
-        "Anton Malmberg",
-        "Asma Rafiq",
-        "Bas Kloppenborg",
-        "CalleR",
-        "Carl Byström",
-        "Christian Nygaard",
-        "Hesham",
-        "Håkan Rosenhorn",
-        "Johan Althoff",
-        "Johan Andersson",
-        "Johan Olsson",
-        "Jonathon Walker",
-        "Karl Pokus",
-        "kenny duong",
-        "Kim Carlbäcker",
-        "Kvadratrot",
-        "Linus Österberg",
-        "Marcus Ekwall",
-        "Mathias Anderssén",
-        "Oskar Flordal",
-        "paer Henrikxon",
-        "Peramanathan Sathyamoorthy",
-        "Rahul Choudhury",
-        "Richard Norqvist"
-      ];
+      var people = ["Aaron Triplett","Adam","Adam Berman","Adam Sadovsky","Alan Strahsburg","Albert Chang","Alec Dara-Abrams","Alex Kin","Alex(ander) Craig","Alexander Malitsky","Alla Il'chuk","Alpesh","Alvin Lai","amir farrahi","Amish","Amit Bharadwaj","Amogh R Rao","Anbuselvan Periannan","Andreas","Andres Osorio Plata","Andrew Glover","Andrew Zavgorodniy","Anil Srivastava","Anirban Kundu","Anna Kucheinyk","Anna Vasilko","Anthony Vella","Anton Kondrashenkov","Anuj Jaiswal","Arie Radilla","Art","Arun Kumar","Arup","Arvind","Arvind Raj","Ashish","Ashwin Vaghasia","Austin Wu","Avina","Banit","Barbara F","Ben Halverson","bernard van haecke","bfonacier","Bill Overstreet","Bill Rainey","Bo Ericsson","Borys","Brandon Gray","Brian Coburn","Bruno Sanches","C L","Carlos Araya","Carol St.Louis","Cathie Liu","Cemal Sert","Chacko N","chakradhar","Charles d'Harcourt","Charles Tung","chase chou","Chien-Chung Chang","Chitra Tatachar","Chris","Chris","Chris Forno","Chris Zankel","Christopher Jesudurai","Chuck H","Cliff Yap","Collin Green","Curtis Schumacher","Dan Baran","Dan Bikle","Dan Carnese","Dan D","Daniel K Griffin","Daniel Mendalka","Daniel Yu","David Hwang","David Lai","David Mihal","Debra Chi","Deepthi K","Dharmi","Dipak","Diwakar Cherukumilli","Don Hejna","Doris Chen","Doug","Dustin Brand","Dylan Kirby","Earl Bingham","Ed Katz","ed young","Eduardo Rodrigues","Elena Tan","Emmanuel","Erin Pangilinan","evans","Evgeny","F4Lens","Firat Karakusoglu","Frank van Gilluwe","Garima Gupta","gary","Gene Escober","Geoffrey Russell","Glen","Gonzalo Gasca Meza","Hari Kunamneni","Hemanth Sridhara Ashalatha","Henri Muurimaa","Henry","Henry","Hongjian Zhao","hoss","hriya","Hung Vo","Hyunjung Lee","Igor Kucheinyk","isaac","ishika","Ismail","J F","Jack Xu","james","James Allen","James Downey","James Feagans","Janice","jason","Jason Campbell","Jason Chuang","Jason Han","Jason Kafalas","Jay Karma","Jeff Brod","Jeff Nainaparampil","Jeff S","Jeff Wang","Jerry Lin","Jerry Si","Joe Barnhart","Joel Tello","John","John","John Bateman","John Hamilton","John Wang","Johnny Kim","Jon","Jon Deibel","Jonah Back","Jonatan K","Joonas Lehtinen","Jorge Jaramillo","Joshua Woodward","Juanita","Justina Cheng","Kaamel Kermaani","Kamran","Kandy Nachimuthu","Karel Vuong","Karl Pohl","Kelvin Wong","Ken Park","Kevin Bjorke","Kevin mo","Kevin Nilson","Organizer","Kevin Tucker","Khanh Dao","Khoa Phan","Ki Lim","Kris Carlsen","Kristen Brann","Ksenia Pachurova","Kuliantxo","Kumar","Kunfeng Chen","Kyrylo Kochubyey","Lars Hidde","Laura Klemme","Lauren Kahn","lawrence","Leif Åstrand","leonard cheong","Lingan","Lionel","Lou de los Re","Lu Zhang","Luke Mertens","M.C. Wilson","MacLane Wilkison","Maksym Taran","Manish Chhabra","Manu Suryavansh","Manuel Carrasco","Marcello Damasceno","Mariano Pardo","mario","Assistant Organizer","mariya","Mark Scholtz","markd","Martin Kuba","Matt Phillips","Matthew Hancock","Meenakshi Ramamoorthi","Melody","Michael","Michael Adams","Michael Carter","Co-Organizer","Michael Schoendorf","michelle","Mike","mkolosick","Mrugen Deshmukh","NancyPhan","Nathan Thomas Mosher","Never Follow","Newton C","Nikita Beloglazov","Nikki L","noa grant","Oleg","Oliver Zhang","p","P.V.","Patrick Thompson","Paul Lee","Pavan Shivaram","Perry Chow","Peter","Peter","Peter Chaivre","Peter O'Connor","Peter Simanyi","Peter Vanderbilt","Peter Zen","Prashanth Govindaraj","Praveen Gupta","Praveen Kumar","preeti","Promila","Rahul Agarwal","Rahul Choudhury","Raisa","Rajan","Rajiv Gupta","Raman Aleksandrovich","Ranjith Tellakula","Raphael Kochuvaried","Ravi Theja Mittapalli","Reshad","Rich Gossweiler","Richard Yee","Rick Evans","Rob","Rob","Robert Bellandi","Roni Michaels","Roy","Roy Arsan","ruchita rathi","Ruslan","Sam","Samir Savla","Sandeep Adwankar","Sanjay Shroff","santhosh","Sarada Pachalla","Sarah Ross","Sasmita","Scott Evans","Senthilnathan Vadivel","Serg Limich","Shashanka Panuganti","Shay Nir","Shirley","Shishir G","Shyam Ravindranathan","shyamal bapodara","sina","Sokthea D. Mov","solomon wu","Son Thai","Soo Hwan Park","Spicy Zinc","Srikanth Kavoori","Steve Chan","Steve Pimen","Suhas","Sumit","Sunil Sabat","Suresh","Suresh Govindaraj","suresh koya","Sushmita Dey","tex","Thomas","Tomas Rokicki","Travis kim","Varun Stifler-Arora","Vasishta Kyatham","Victor Chugunov","Victor Korshun","vidya","Viji Balas","Vikram Angrish","Vipul Nadoda","Vlad Nakonechnyi","wang","Wen Lee","Wenbo Zhu","William H","Wing","Young Keun Park","Yow-Hann","Yun"];
 
-      _.each(people, function(person) {
+      _.each(_.first(people, 42), function(person) {
           People.insert({
             name: person,
             plusones: 0,
